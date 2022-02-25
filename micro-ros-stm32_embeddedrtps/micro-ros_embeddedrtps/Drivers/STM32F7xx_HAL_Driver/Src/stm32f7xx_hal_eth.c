94fd985852c006eca4a590af3ef80d52a31c9e94
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-02-25 19:50:57 +0900

    8. Make sure that macinit.MulticastFramesFilter is set to ETH_MULTICASTFRAMESFILTER_NONE. Usually this configuration is set in Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_eth.c:1878 and by default its value is ETH_MULTICASTFRAMESFILTER_PERFECT. Probably this is modified automatically every time code is generated.
    https://github.com/mROS-base/micro_ros_stm32cubemx_utils/blob/galactic/embeddedrtps.md

--- micro-ros-stm32_embeddedrtps/micro-ros_embeddedrtps/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_eth.c
+++ micro-ros-stm32_embeddedrtps/micro-ros_embeddedrtps/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_eth.c
@@ -1875,7 +1875,7 @@ static void ETH_MACDMAConfig(ETH_HandleTypeDef *heth, uint32_t err)
   macinit.BroadcastFramesReception = ETH_BROADCASTFRAMESRECEPTION_ENABLE;
   macinit.DestinationAddrFilter = ETH_DESTINATIONADDRFILTER_NORMAL;
   macinit.PromiscuousMode = ETH_PROMISCUOUS_MODE_DISABLE;
-  macinit.MulticastFramesFilter = ETH_MULTICASTFRAMESFILTER_PERFECT;
+  macinit.MulticastFramesFilter = ETH_MULTICASTFRAMESFILTER_NONE;
   macinit.UnicastFramesFilter = ETH_UNICASTFRAMESFILTER_PERFECT;
   macinit.HashTableHigh = 0x0;
   macinit.HashTableLow = 0x0;
